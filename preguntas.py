0407a4b8062cece3e27db05e70dafad2d8014417
Santiago Alexis Valencia Zapata <svalenciaz@unal.edu.co>
2022-06-21 02:18:54 -0500

    Second Try

--- preguntas.py
+++ preguntas.py
@@ -143,7 +143,7 @@ def pregunta_04():
     """
 
     # Importe mean_squared_error
-    from ____ import ____
+    from sklearn.metrics import mean_squared_error
 
     # Obtenga el pipeline optimo de la pregunta 3.
     gridSearchCV = pregunta_03()
@@ -152,20 +152,20 @@ def pregunta_04():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el modelo con los conjuntos de entrenamiento y prueba.
-    y_train_pred = ____.____(____)
-    y_test_pred = ____.____(____)
+    y_train_pred = gridSearchCV.predict(X_train)
+    y_test_pred = gridSearchCV.predict(X_test)
 
     # Compute el error cuadratico medio de entrenamiento y prueba. Redondee los
     # valores a dos decimales.
 
-    mse_train = ____(
-        _____,
-        _____,
+    mse_train = mean_squared_error(
+        y_train,
+        y_train_pred,
     ).round(2)
 
-    mse_test = ____(
-        _____,
-        _____,
+    mse_test = mean_squared_error(
+        y_test,
+        y_test_pred,
     ).round(2)
 
     # Retorne el error cuadrático medio para entrenamiento y prueba
